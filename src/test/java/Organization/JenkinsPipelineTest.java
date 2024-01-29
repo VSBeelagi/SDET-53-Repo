8b15ed24918b24eb2f8e1aacbdc0e9f19aa97320
VSBeelagi <vijayalaxmibeelagi10@gmail.com>
2024-01-29 16:28:53 +0530

    Jenkins Pipeline

---
+++ src/test/java/Organization/JenkinsPipelineTest.java
@@ -0,0 +1,12 @@
+package Organization;
+
+import org.testng.annotations.Test;
+
+public class JenkinsPipelineTest {
+
+	@Test
+	public void jenkinsPipelineTest()
+	{
+		System.out.println("---pipeline---");
+	}
+}
